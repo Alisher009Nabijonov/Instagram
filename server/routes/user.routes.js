bfcb17b701214af7dda9e543d30c2deebac5dabc
Muhammadali-fr <boburjonr934@gmail.com>
2025-02-21 09:03:45 +0500

    get user by id

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers } = require("../controllers/users.controller");
+const { getAllUsers, getUser } = require("../controllers/users.controller");
 
 router.get("/getAllUsers", getAllUsers);
+router.get("/getUser/:id", getUser);
 
 module.exports = router;
