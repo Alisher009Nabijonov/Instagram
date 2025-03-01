0c4eb78e562ad5f0af01bde76f3e60098bdca82b
Muhammadali-fr <boburjonr934@gmail.com>
2025-03-01 15:17:48 +0500

    User bio

--- client/src/components/Profil.jsx
+++ client/src/components/Profil.jsx
@@ -120,8 +120,8 @@ const Profil = () => {
           <div>
             <img
               src={user ? `http://localhost:5000${user.avatar}` : UserImg}
-              alt=""
-              className="w-30 h-30 rounded-full cursor-pointer"
+              alt="User img"
+              className="w-30 h-30 rounded-full cursor-pointer object-cover"
             />
           </div>
           <div>
@@ -174,6 +174,9 @@ const Profil = () => {
 
             <p className="pt-5 text-sm font-extrabold">
               {user ? user.name : "Name"}
+            </p>
+            <p className=" pt-5 text-sm text-gray-300">
+              {user ? (user.bio) : ("You bio will be showed here.")}
             </p>
           </div>
         </div>
